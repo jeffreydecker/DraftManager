a8fb493fe64a33dd32a7ff58be1975b249f3a74a
Jeffrey <deckerjeffreyr@gmail.com>
2019-02-17 00:37:28 -0800

    Promise and Async style queries to get rankings and projections and add them to the db. We still need to do a better job handling errors and should probably return a promise from the fns and not mark them as async

--- app/models/player.js
+++ app/models/player.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 var PlayerSchema = new Schema({
   _id   : String,   // FantasyPros player id
   name  : String,
-  rank  : {type: Number, default: null},   // Overall ranking according to FantasyPros concensus
+  rank  : {type: Number, default: Number.MAX_SAFE_INTEGER},   // Overall ranking according to FantasyPros concensus
   team  : String,
   pos   : String,
   // TODO - may want to reconsider this model to reference projections
--- app/routes/scrape.routes.js
+++ app/routes/scrape.routes.js
@@ -31,6 +31,8 @@ router.route('/update/players')
   try {
     // Rankings
     await getRankings()
+    await getHitterProjections()
+    await getPitcherProjections()
 
     return res.status(200).json({msg: "Rankings Updated"})
   } catch(err) {
@@ -41,7 +43,7 @@ router.route('/update/players')
 async function getRankings() {
     // Rankings
     // Get rankings html and load it into cheerio
-    var rankingsUrl = 'https://www.fantasypros.com/mlb/rankings/overall.php';
+    var rankingsUrl = 'https://www.fantasypros.com/mlb/rankings/overall.php'
     let rankingsHtml = await rp(rankingsUrl)
     var $ = cheerio.load(rankingsHtml)
 
@@ -60,7 +62,7 @@ async function getRankings() {
           team : $(this).attr('data-team'),
           pos : $(this).attr('data-pos')
         },
-        {upsert : true}, // If not found, create a new one
+        { upsert : true, , new : true }, // If not found, create a new one
       )
       playerQueries.push(playerQuery)
     });
@@ -70,6 +72,146 @@ async function getRankings() {
         await query.exec()
     })
 }
+
+async function getHitterProjections() {
+  var url = 'https://www.fantasypros.com/mlb/projections/hitters.php'
+  var html = await rp(url)
+  var $ = cheerio.load(html)
+
+  var players = [];
+  $('#data tbody tr').each(function (index, element) {
+    // Kind of sketchy way to get the fantasypros playerId from the dom for each row.
+    // These have a second class fp-id-<id> so we are grabbing the id part from that.
+    var playerId = $($(this).find('.fp-player-link').get(0)).attr('class').split('-').pop();
+    var playerName = $(this).find('.player-name').text();
+    var team = $(this).find('small a').text();
+    var positions = $(this).find('small').text().split(' - ').pop().replace(')', '');
+    var projections = $(this).find('td');
+    players.push({
+      id : playerId,
+      name : playerName,
+      team : team,
+      positions : positions,
+      atBats : $(projections.get(1)).text(),
+      runs : $(projections.get(2)).text(),
+      homeRuns : $(projections.get(3)).text(),
+      rbi : $(projections.get(4)).text(),
+      steals : $(projections.get(5)).text(),
+      average : $(projections.get(6)).text(),
+      obp : $(projections.get(7)).text(),
+      hits : $(projections.get(8)).text(),
+      doubles : $(projections.get(9)).text(),
+      tripples : $(projections.get(10)).text(),
+      walks : $(projections.get(11)).text(),
+      strikeouts : $(projections.get(12)).text(),
+      slugging : $(projections.get(13)).text(),
+      ops : $(projections.get(14)).text(),
+    })
+  })
+
+  players.forEach(async (player) => {
+      let projection = await HitterProjection.findOneAndUpdate(
+        {_player : player.id },
+        { // Find existing player by id
+          _player : player.id,
+          atBats : player.atBats,
+          runs : player.runs,
+          homeRuns : player.homeRuns,
+          rbi : player.rbi,
+          steals : player.steals,
+          average : player.average,
+          obp : player.obp,
+          hits : player.hits,
+          doubles : player.doubles,
+          tripples : player.tripples,
+          walks : player.walks,
+          strikeouts : player.strikeouts,
+          slugging : player.slugging,
+          ops : player.ops,
+        },
+        { upsert : true, new : true },
+      ).exec()
+
+      let playerUpdate = await Player.findOneAndUpdate(
+        { _id : projection._player },
+        { _id : projection._player,
+          name : player.name,
+          team : player.team,
+          pos : player.positions,
+          hittingProjections: projection._id },
+        { upsert : true, new : true },
+      ).exec();
+  })
+}
+
+async function getPitcherProjections() {
+  var url = 'https://www.fantasypros.com/mlb/projections/pitchers.php';
+  var html = await rp(url)
+  var $ = cheerio.load(html)
+
+  var players = [];
+  $('#data tbody tr').each(function (index, element) {
+    var playerId = $($(this).find('.fp-player-link').get(0)).attr('class').split('-').pop();
+    var playerName = $(this).find('.player-name').text();
+    var team = $(this).find('small a').text();
+    var positions = $(this).find('small').text().split(' - ').pop().replace(')', '');
+    var projections = $(this).find('td');
+
+    players.push({ // Find existing player by id
+      id : playerId,
+      name : playerName,
+      team : team,
+      positions : positions,
+      innings : $(projections.get(1)).text(),
+      strikeouts : $(projections.get(2)).text(),
+      wins : $(projections.get(3)).text(),
+      saves : $(projections.get(4)).text(),
+      era : $(projections.get(5)).text(),
+      whip : $(projections.get(6)).text(),
+      earnedRuns : $(projections.get(7)).text(),
+      hits : $(projections.get(8)).text(),
+      walks : $(projections.get(9)).text(),
+      homeRuns : $(projections.get(10)).text(),
+      games : $(projections.get(11)).text(),
+      starts : $(projections.get(12)).text(),
+      losses : $(projections.get(13)).text(),
+      completeGames : $(projections.get(14)).text(),
+    });
+  })
+
+  players.forEach(async (player) => {
+    let projection = await PitcherProjection.findOneAndUpdate(
+      {_player : player.id}, { // Find existing player by id
+        _player : player.id,
+        innings : player.innings,
+        strikeouts : player.strikeouts,
+        wins : player.wins,
+        saves : player.saves,
+        era : player.era,
+        whip : player.whip,
+        earnedRuns : player.earnedRuns,
+        hits : player.hits,
+        walks : player.walks,
+        homeRuns : player.homeRuns,
+        games : player.games,
+        starts : player.starts,
+        losses : player.losses,
+        completeGames : player.completeGames,
+      }, { upsert : true, new : true}, // If not found, create a new one
+    ).exec()
+    
+    let updatedPlayer = Player.findOneAndUpdate(
+      { _id : projection._player },
+      { _id : projection._player,
+        name : player.name,
+        team : player.team,
+        pos : player.positions,
+        pitchingProjections: projection._id },
+      { upsert : true, new : true },
+    ).exec()
+  })
+};
+
 
 router.route('/rankings')
 .get((req, res) => {
